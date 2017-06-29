0a57bf9472bfb29e695e1cf3c9bd57f33bcd2d7e
jessieeeee <lyj1246505807@gmail.com>
2017-06-29 11:29:09 +0800

    修改bug与自定义属性添加并调整目录结构

--- app/src/main/java/journeycalendar/jessie/com/journeycalendar/MainActivity.java
+++ app/src/main/java/journeycalendar/jessie/com/journeycalendar/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ImageView;
